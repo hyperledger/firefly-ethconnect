eca5a3471bd37ba064bffd726876010e0a6956a2
Peter Broadhurst <peter.broadhurst@consensys.net>
2018-07-20 13:50:46 -0400

    Ensure shutdown of server

--- internal/kldwebhooks/webhooksbridge.go
+++ internal/kldwebhooks/webhooksbridge.go
@@ -391,15 +391,18 @@ func (w *WebhooksBridge) Start() (err error) {
 	}
 
 	// Wait until Kafka is up before we listen
+	running := true
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		for w.kafka.Producer() == nil {
+		for w.kafka.Producer() == nil && running {
 			time.Sleep(500)
 		}
-		log.Printf("Listening on %s", w.srv.Addr)
-		if err := w.srv.ListenAndServe(); err != nil {
-			log.Errorf("Listening ended with: %s", err)
+		if running {
+			log.Printf("Listening on %s", w.srv.Addr)
+			if err := w.srv.ListenAndServe(); err != nil {
+				log.Errorf("Listening ended with: %s", err)
+			}
 		}
 		wg.Done()
 	}()
@@ -413,6 +416,7 @@ func (w *WebhooksBridge) Start() (err error) {
 	w.srv.Shutdown(ctx)
 	defer cancel()
 
+	running = false
 	wg.Wait()
 	return
 }

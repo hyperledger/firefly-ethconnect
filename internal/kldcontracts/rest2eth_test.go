a54eb42c11394ccb72d65ba2d716490d5fcbf6d6
Jim Zhang <jim.zhang@kaleido.io>
2020-04-18 12:10:38 -0400

    Fixed tests and added to openapi spec

--- internal/kldcontracts/rest2eth_test.go
+++ internal/kldcontracts/rest2eth_test.go
@@ -1051,6 +1051,7 @@ func TestCallMethodSuccess(t *testing.T) {
 
 	assert.Equal(200, res.Result().StatusCode)
 	assert.Equal("eth_call", mockRPC.capturedMethod)
+	assert.Equal("latest", mockRPC.capturedArgs[1])
 	var reply map[string]interface{}
 	err := json.NewDecoder(res.Result().Body).Decode(&reply)
 	assert.NoError(err)
@@ -1069,7 +1070,7 @@ func TestCallReadOnlyMethodViaPOSTSuccess(t *testing.T) {
 	to := "0x567a417717cb6c59ddc1035705f02c0fd1ab1872"
 	dispatcher := &mockREST2EthDispatcher{}
 	_, mockRPC, router, res, _ := newTestREST2EthAndMsg(dispatcher, "", to, map[string]interface{}{})
-	req := httptest.NewRequest("POST", "/contracts/"+to+"/get", strings.NewReader("kld-option=12345"))
+	req := httptest.NewRequest("POST", "/contracts/"+to+"/get?kld-option=12345", bytes.NewReader([]byte{}))
 	mockRPC.result = "0x000000000000000000000000000000000000000000000000000000000001e2400000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000774657374696e6700000000000000000000000000000000000000000000000000"
 	router.ServeHTTP(res, req)
 
--- internal/kldopenapi/abi2swagger.go
+++ internal/kldopenapi/abi2swagger.go
@@ -362,6 +362,18 @@ func (c *ABI2Swagger) getCommonParameters() map[string]spec.Parameter {
 			Type: "string",
 		},
 	}
+	params["optionParam"] = spec.Parameter{
+		ParamProps: spec.ParamProps{
+			Description:     "Used when kld-call is true, to specify the block number for the eth_call request to target. Empty or 'latest' to indicate the latest mined block. A number or a hex string to indicate a historical block height - 'x-kaleido-option' header can also be used",
+			Name:            "kld-option",
+			In:              "query",
+			Required:        false,
+			AllowEmptyValue: false,
+		},
+		SimpleSchema: spec.SimpleSchema{
+			Type: "string",
+		},
+	}
 	return params
 }
 
@@ -379,6 +391,7 @@ func (c *ABI2Swagger) addCommonParams(op *spec.Operation, isPOST bool, isConstru
 	privateForParam, _ := spec.NewRef("#/parameters/privateForParam")
 	privacyGroupIdParam, _ := spec.NewRef("#/parameters/privacyGroupIdParam")
 	registerParam, _ := spec.NewRef("#/parameters/registerParam")
+	optionParam, _ := spec.NewRef("#/parameters/optionParam")
 	op.Parameters = append(op.Parameters, spec.Parameter{
 		Refable: spec.Refable{
 			Ref: fromParam,
@@ -418,6 +431,11 @@ func (c *ABI2Swagger) addCommonParams(op *spec.Operation, isPOST bool, isConstru
 		op.Parameters = append(op.Parameters, spec.Parameter{
 			Refable: spec.Refable{
 				Ref: privateForParam,
+			},
+		})
+		op.Parameters = append(op.Parameters, spec.Parameter{
+			Refable: spec.Refable{
+				Ref: optionParam,
 			},
 		})
 		if c.orionPrivateAPI {

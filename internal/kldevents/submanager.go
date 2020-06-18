hyperledger/firefly-ethconnect

commit 1b66f3047d1ff7d06e8874ecb804f56592b8b872

Add Name to Events, CustomName to Subscription (#71)
* Add name to Events

* Allow sol 0.6.8, go1.14 tweaks for json number parsing

* allows for solc0.6.8
* go1.14 has changes to json `Number` treatment. This resulted in some
of our negative tests returning a different error message due to
alphabet input for "bad number"

* Change logEntry tx index type to hex uint

For correctness and also because go1.14 is messy
when parsing incorrect numbers. For us, when parsing our test events
from our file that has `0x0` into a "Number", the resulting slice does
not have the right data.

ref: https://github.com/golang/go/issues/14702
https://github.com/ethereum/go-ethereum/blob/4bcc0a37ab70cb79b16893556cffdaad6974e7d8/core/types/log.go#L47

* Add CustomName to Subscriptions

* Fix json for event name

* Add tests with event stream name in smartcontract gw

* Sub auto-generated summary, accept name from end user

* Sets name to whatever is provided by end user, if present in the body
* Sets name to `summary`, if one is not provided by end user. summary is
auto generated and not returned in the API response
* Removed a comment from test file based on review comment

internal/kldevents/submanager.go
@@ -52,7 +52,7 @@ type SubscriptionManager interface {
 	SuspendStream(ctx context.Context, id string) error
 	ResumeStream(ctx context.Context, id string) error
 	DeleteStream(ctx context.Context, id string) error
-	AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock string) (*SubscriptionInfo, error)
+	AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock, name string) (*SubscriptionInfo, error)
 	Subscriptions(ctx context.Context) []*SubscriptionInfo
 	SubscriptionByID(ctx context.Context, id string) (*SubscriptionInfo, error)
 	DeleteSubscription(ctx context.Context, id string) error
@@ -125,7 +125,7 @@ func (s *subscriptionMGR) Subscriptions(ctx context.Context) []*SubscriptionInfo
 }
 
 // AddSubscription adds a new subscription
-func (s *subscriptionMGR) AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock string) (*SubscriptionInfo, error) {
+func (s *subscriptionMGR) AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock, name string) (*SubscriptionInfo, error) {
 	i := &SubscriptionInfo{
 		TimeSorted: kldmessages.TimeSorted{
 			CreatedISO8601: time.Now().UTC().Format(time.RFC3339),

internal/kldevents/eventstream_test.go
@@ -180,6 +180,21 @@ func TestBatchSizeCap(t *testing.T) {
 	defer stream.stop()
 
 	assert.Equal(uint64(MaxBatchSize), stream.spec.BatchSize)
+	assert.Equal("", stream.spec.Name)
+}
+
+func TestStreamName(t *testing.T) {
+	assert := assert.New(t)
+	_, stream, svr, eventStream := newTestStreamForBatching(
+		&StreamInfo{
+			Name:    "testStream",
+			Webhook: &webhookAction{},
+		}, 200)
+	defer close(eventStream)
+	defer svr.Close()
+	defer stream.stop()
+
+	assert.Equal("testStream", stream.spec.Name)
 }
 
 func TestBlockingBehavior(t *testing.T) {
@@ -405,7 +420,8 @@ func setupTestSubscription(assert *assert.Assertions, sm *subscriptionMGR, strea
 	}
 	addr := kldbind.HexToAddress("0x167f57a13a9c35ff92f0649d2be0e52b4f8ac3ca")
 	ctx := context.Background()
-	s, _ := sm.AddSubscription(ctx, &addr, event, stream.spec.ID, "")
+	subscriptionName := "testSub"
+	s, _ := sm.AddSubscription(ctx, &addr, event, stream.spec.ID, "", subscriptionName)
 	return s
 }
 

internal/kldbind/types.go
@@ -35,6 +35,9 @@ type HexBigInt = hexutil.Big
 // HexUint64 models and serializes uint64
 type HexUint64 = hexutil.Uint64
 
+// HexUint models and serializes uint
+type HexUint = hexutil.Uint
+
 // ABIEvent is an event on the ABI
 type ABIEvent = abi.Event
 

internal/kldcontracts/rest2eth.go
@@ -432,7 +432,10 @@ func (r *rest2eth) subscribeEvent(res http.ResponseWriter, req *http.Request, ad
 		address := common.HexToAddress(addrStr)
 		addr = &address
 	}
-	sub, err := r.subMgr.AddSubscription(req.Context(), addr, abiEvent, streamID, fromBlock)
+	// if the end user provided a name for the subscription, use it
+	// If not provided, it will be set to a system-generated summary
+	name := r.fromBodyOrForm(req, body, "name")
+	sub, err := r.subMgr.AddSubscription(req.Context(), addr, abiEvent, streamID, fromBlock, name)
 	if err != nil {
 		r.restErrReply(res, req, err, 400)
 		return

internal/kldcontracts/rest2eth_test.go
@@ -154,7 +154,7 @@ func (m *mockSubMgr) ResumeStream(ctx context.Context, id string) error {
 	return m.err
 }
 func (m *mockSubMgr) DeleteStream(ctx context.Context, id string) error { return m.err }
-func (m *mockSubMgr) AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock string) (*kldevents.SubscriptionInfo, error) {
+func (m *mockSubMgr) AddSubscription(ctx context.Context, addr *kldbind.Address, event *kldbind.ABIEvent, streamID, initialBlock, name string) (*kldevents.SubscriptionInfo, error) {
 	m.capturedAddr = addr
 	return m.sub, m.err
 }
@@ -1247,7 +1247,7 @@ func TestSubscribeNoAddressSuccess(t *testing.T) {
 	dispatcher := &mockREST2EthDispatcher{}
 	r, _, router := newTestREST2Eth(dispatcher)
 	sm := &mockSubMgr{
-		sub: &kldevents.SubscriptionInfo{ID: "sub1"},
+		sub: &kldevents.SubscriptionInfo{ID: "sub1", Name: "stream-without-address"},
 	}
 	r.subMgr = sm
 	bodyBytes, _ := json.Marshal(&map[string]string{
@@ -1262,6 +1262,7 @@ func TestSubscribeNoAddressSuccess(t *testing.T) {
 	err := json.NewDecoder(res.Result().Body).Decode(&reply)
 	assert.NoError(err)
 	assert.Equal("sub1", reply.ID)
+	assert.Equal("stream-without-address", reply.Name)
 	assert.Nil(sm.capturedAddr)
 }
 

internal/kldcontracts/smartcontractgw_test.go
@@ -1594,7 +1594,7 @@ func TestAddStreamNoSubMgr(t *testing.T) {
 
 func TestAddStreamOK(t *testing.T) {
 	assert := assert.New(t)
-	spec := &kldevents.StreamInfo{Type: "webhook"}
+	spec := &kldevents.StreamInfo{Type: "webhook", Name: "stream-1"}
 	b, _ := json.Marshal(spec)
 	req := httptest.NewRequest("POST", kldevents.StreamPathPrefix, bytes.NewReader(b))
 	res := httptest.NewRecorder()
@@ -1607,6 +1607,7 @@ func TestAddStreamOK(t *testing.T) {
 	json.NewDecoder(res.Body).Decode(&newSpec)
 	assert.Equal(200, res.Result().StatusCode)
 	assert.Equal("webhook", newSpec.Type)
+	assert.Equal("stream-1", newSpec.Name)
 	s.Shutdown()
 }
 
@@ -1653,15 +1654,15 @@ func TestListStreams(t *testing.T) {
 
 	mockSubMgr := &mockSubMgr{
 		streams: []*kldevents.StreamInfo{
-			&kldevents.StreamInfo{
+			{
 				TimeSorted: kldmessages.TimeSorted{
 					CreatedISO8601: time.Now().UTC().Format(time.RFC3339),
-				}, ID: "earlier",
+				}, ID: "earlier", Name: "stream-1",
 			},
-			&kldevents.StreamInfo{
+			{
 				TimeSorted: kldmessages.TimeSorted{
 					CreatedISO8601: time.Now().UTC().Add(1 * time.Hour).Format(time.RFC3339),
-				}, ID: "later",
+				}, ID: "later", Name: "stream-2",
 			},
 		},
 	}
@@ -1670,7 +1671,9 @@ func TestListStreams(t *testing.T) {
 	assert.Equal(200, res.Result().StatusCode)
 	assert.Equal(2, len(results))
 	assert.Equal("later", results[0].ID)
+	assert.Equal("stream-2", results[0].Name)
 	assert.Equal("earlier", results[1].ID)
+	assert.Equal("stream-1", results[1].Name)
 }
 
 func TestListSubs(t *testing.T) {

internal/kldeth/txn_test.go
@@ -58,8 +58,8 @@ func (r *testRPCClient) CallContext(ctx context.Context, result interface{}, met
 }
 
 const (
-	simpleStorage = "pragma solidity >=0.4.22 <0.6.0;\n\ncontract simplestorage {\nuint public storedData;\n\nconstructor(uint initVal) public {\nstoredData = initVal;\n}\n\nfunction set(uint x) public {\nstoredData = x;\n}\n\nfunction get() public view returns (uint retVal) {\nreturn storedData;\n}\n}"
-	twoContracts  = "pragma solidity >=0.4.22 <0.6.0;\n\ncontract contract1 {function f1() public pure returns (uint retVal) {\nreturn 1;\n}\n}\n\ncontract contract2 {function f2() public pure returns (uint retVal) {\nreturn 2;\n}\n}"
+	simpleStorage = "pragma solidity >=0.4.22 <0.6.9;\n\ncontract simplestorage {\nuint public storedData;\n\nconstructor(uint initVal) public {\nstoredData = initVal;\n}\n\nfunction set(uint x) public {\nstoredData = x;\n}\n\nfunction get() public view returns (uint retVal) {\nreturn storedData;\n}\n}"
+	twoContracts  = "pragma solidity >=0.4.22 <0.6.9;\n\ncontract contract1 {function f1() public pure returns (uint retVal) {\nreturn 1;\n}\n}\n\ncontract contract2 {function f2() public pure returns (uint retVal) {\nreturn 2;\n}\n}"
 )
 
 func TestNewContractDeployTxnSimpleStorage(t *testing.T) {
@@ -446,7 +446,7 @@ func testComplexParam(t *testing.T, solidityType string, val interface{}, expect
 	assert := assert.New(t)
 
 	var msg kldmessages.DeployContract
-	msg.Solidity = "pragma solidity >=0.4.22 <0.6.0; contract test {constructor(" + solidityType + " p1) public {}}"
+	msg.Solidity = "pragma solidity >=0.4.22 <0.6.9; contract test {constructor(" + solidityType + " p1) public {}}"
 	msg.Parameters = []interface{}{val}
 	msg.From = "0xAA983AD2a0e0eD8ac639277F37be42F2A5d2618c"
 	msg.Nonce = "123"

internal/kldevents/eventstream.go
@@ -55,6 +55,7 @@ const (
 type StreamInfo struct {
 	kldmessages.TimeSorted
 	ID                   string         `json:"id"`
+	Name                 string         `json:"name,omitempty"`
 	Path                 string         `json:"path"`
 	Suspended            bool           `json:"suspended"`
 	Type                 string         `json:"type,omitempty"`
@@ -422,7 +423,7 @@ func (a *eventStream) performActionWithRetry(batchNumber uint64, events []*event
 	complete := false
 	for !a.suspendOrStop() && !complete {
 		if attempt > 0 {
-			log.Infof("%s: Watiting %.2fs before re-attempting batch %d", a.spec.ID, delay.Seconds(), batchNumber)
+			log.Infof("%s: Waiting %.2fs before re-attempting batch %d", a.spec.ID, delay.Seconds(), batchNumber)
 			time.Sleep(delay)
 			delay = time.Duration(float64(delay) * a.backoffFactor)
 		}

commit 85e3065f68b1fdca8efcefb4e42686a7f57b113a

Set subscription name before creating subscr info
The name was getting dropped in the chain of calls. Set the name that
is passed down from the caller before creating the subscription info.

Ref: https://github.com/kaleido-io/ethconnect/issues/68
Fixes a bug introduced in https://github.com/kaleido-io/ethconnect/pull/71

internal/kldevents/submanager.go
@@ -135,6 +135,10 @@ func (s *subscriptionMGR) AddSubscription(ctx context.Context, addr *kldbind.Add
 		Stream: streamID,
 	}
 	i.Path = SubPathPrefix + "/" + i.ID
+	// Set any user supplied a name for the subscription
+	if name != "" {
+		i.Name = name
+	}
 	// Check initial block number to subscribe from
 	if initialBlock == "" || initialBlock == FromBlockLatest {
 		i.FromBlock = FromBlockLatest

internal/kldevents/eventstream_test.go
@@ -369,7 +369,7 @@ func TestBuildup(t *testing.T) {
 
 }
 
-func setupTestSubscription(assert *assert.Assertions, sm *subscriptionMGR, stream *eventStream) *SubscriptionInfo {
+func setupTestSubscription(assert *assert.Assertions, sm *subscriptionMGR, stream *eventStream, subscriptionName string) *SubscriptionInfo {
 	log.SetLevel(log.DebugLevel)
 	testDataBytes, err := ioutil.ReadFile("../../test/simplevents_logs.json")
 	assert.NoError(err)
@@ -393,26 +393,26 @@ func setupTestSubscription(assert *assert.Assertions, sm *subscriptionMGR, strea
 	event := &kldbind.ABIEvent{
 		Name: "Changed",
 		Inputs: []kldbind.ABIArgument{
-			kldbind.ABIArgument{
+			{
 				Name:    "from",
 				Type:    kldbind.ABITypeKnown("address"),
 				Indexed: true,
 			},
-			kldbind.ABIArgument{
+			{
 				Name:    "i",
 				Type:    kldbind.ABITypeKnown("int64"),
 				Indexed: true,
 			},
-			kldbind.ABIArgument{
+			{
 				Name:    "s",
 				Type:    kldbind.ABITypeKnown("string"),
 				Indexed: true,
 			},
-			kldbind.ABIArgument{
+			{
 				Name: "h",
 				Type: kldbind.ABITypeKnown("bytes32"),
 			},
-			kldbind.ABIArgument{
+			{
 				Name: "m",
 				Type: kldbind.ABITypeKnown("string"),
 			},
@@ -420,7 +420,6 @@ func setupTestSubscription(assert *assert.Assertions, sm *subscriptionMGR, strea
 	}
 	addr := kldbind.HexToAddress("0x167f57a13a9c35ff92f0649d2be0e52b4f8ac3ca")
 	ctx := context.Background()
-	subscriptionName := "testSub"
 	s, _ := sm.AddSubscription(ctx, &addr, event, stream.spec.ID, "", subscriptionName)
 	return s
 }
@@ -438,7 +437,8 @@ func TestProcessEventsEnd2End(t *testing.T) {
 	sm.db, _ = kldkvstore.NewLDBKeyValueStore(dir)
 	defer svr.Close()
 
-	s := setupTestSubscription(assert, sm, stream)
+	s := setupTestSubscription(assert, sm, stream, "mySubName")
+	assert.Equal("mySubName", s.Name)
 
 	// We expect three events to be sent to the webhook
 	// With the default batch size of 1, that means three separate requests
@@ -492,7 +492,8 @@ func TestCheckpointRecovery(t *testing.T) {
 		wg.Done()
 	}()
 
-	s := setupTestSubscription(assert, sm, stream)
+	s := setupTestSubscription(assert, sm, stream, "myTestSub")
+	assert.Equal("myTestSub", s.Name)
 
 	for {
 		time.Sleep(1 * time.Millisecond)
@@ -550,7 +551,7 @@ func TestWithoutCheckpointRecovery(t *testing.T) {
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	s := setupTestSubscription(assert, sm, stream)
+	s := setupTestSubscription(assert, sm, stream, "")
 
 	var initialEndBlock string
 	sub := sm.subscriptions[s.ID]
@@ -591,7 +592,7 @@ func TestMarkStaleOnError(t *testing.T) {
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	s := setupTestSubscription(assert, sm, stream)
+	s := setupTestSubscription(assert, sm, stream, "")
 	sm.subscriptions[s.ID].filterStale = false
 
 	sub := sm.subscriptions[s.ID]
@@ -651,7 +652,7 @@ func TestStoreCheckpointStoreError(t *testing.T) {
 		}
 		wg.Done()
 	}()
-	setupTestSubscription(assert, sm, stream)
+	setupTestSubscription(assert, sm, stream, "")
 	wg.Wait()
 
 	stream.suspend()
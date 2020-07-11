hyperledger/firefly-ethconnect

commit 821c1682ede920ed5534a4c791d7f91310358182

Update EventStream (http PATCH) (#73)
* Add command to gen html for coverage

* Update inflight count only if we processed event

processing is skipped if we are paused/stopped. Hence check that we
processed the event in the previous section before decrementing the
in-flight counter

* Adds a PATCH route to update event streams

* Event streams could be updated while batches of events are currently
being processed. The existing logic around concurrent handling of batch
processing of events and any pause/stopping of event streams ensures
that batch processing exits as soon as a pause/stop event is detected.
This PR adds an additional context (and a cancel function) to the event
stream spec so that an update to the event stream can signal all the
batch processing related go routines to invoke the cancel function
on the context (via select)
* All fields in the event stream except the ID are allowed to be updated

Addresses one of the items in: https://github.com/kaleido-io/ethconnect/issues/68

* remove spurious entry added in Makefile

* Use a empty struct channel to signal a cancelation, instead of ctx

* Use an empty struct channel that can be closed to signal that the
update of the stream is underway. The handler go routines select on the
updateInterrupt ch so that they are woken up when the update is
underway and can exit
* The following go routines & functions react to the updateInterrupt
channel - event poller, batch processor, batch dispatcher, individual
process batch (Blocking and sleeps until timeout),
perform webhook action (blocking and sleeps until timeout)
* adds pre/post update helpers notify routines waiting on the condition
variable and to reset flags respectively
* adds a helper function to kick off a fresh set of go routines (used
by resume and at the end of update)

* Check error returned from subMgr updateStream call

* Remove unused dispatcherDone bool flag

internal/kldcontracts/smartcontractgw.go
@@ -115,6 +115,7 @@ func (g *smartContractGW) AddRoutes(router *httprouter.Router) {
 	router.GET("/gateways/:gateway_lookup", g.getRemoteRegistrySwaggerOrABI)
 	router.GET("/g/:gateway_lookup", g.getRemoteRegistrySwaggerOrABI)
 	router.POST(kldevents.StreamPathPrefix, g.withEventsAuth(g.createStream))
+	router.PATCH(kldevents.StreamPathPrefix, g.withEventsAuth(g.updateStream))
 	router.GET(kldevents.StreamPathPrefix, g.withEventsAuth(g.listStreamsOrSubs))
 	router.GET(kldevents.SubPathPrefix, g.withEventsAuth(g.listStreamsOrSubs))
 	router.GET(kldevents.StreamPathPrefix+"/:id", g.withEventsAuth(g.getStreamOrSub))
@@ -642,6 +643,41 @@ func (g *smartContractGW) createStream(res http.ResponseWriter, req *http.Reques
 	newSpec, err := g.sm.AddStream(req.Context(), &spec)
 	if err != nil {
 		g.gatewayErrReply(res, req, err, 400)
+		return
+	}
+
+	status := 200
+	log.Infof("<-- %s %s [%d]", req.Method, req.URL, status)
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(status)
+	enc := json.NewEncoder(res)
+	enc.SetIndent("", "  ")
+	enc.Encode(&newSpec)
+}
+
+// updateStream updates a stream
+func (g *smartContractGW) updateStream(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	log.Infof("--> %s %s", req.Method, req.URL)
+
+	if g.sm == nil {
+		g.gatewayErrReply(res, req, errors.New(errEventSupportMissing), 405)
+		return
+	}
+
+	streamID := params.ByName("id")
+	_, err := g.sm.StreamByID(req.Context(), streamID)
+	if err != nil {
+		g.gatewayErrReply(res, req, err, 404)
+		return
+	}
+	var spec kldevents.StreamInfo
+	if err := json.NewDecoder(req.Body).Decode(&spec); err != nil {
+		g.gatewayErrReply(res, req, klderrors.Errorf(klderrors.RESTGatewayEventStreamInvalid, err), 400)
+		return
+	}
+	newSpec, err := g.sm.UpdateStream(req.Context(), streamID, &spec)
+	if err != nil {
+		g.gatewayErrReply(res, req, err, 500)
 		return
 	}
 

internal/kldcontracts/smartcontractgw_test.go
@@ -1643,6 +1643,81 @@ func TestAddStreamSubMgrError(t *testing.T) {
 	assert.Regexp("pop", resError.Message)
 }
 
+func TestUpdateStreamNoSubMgr(t *testing.T) {
+	assert := assert.New(t)
+	res := testGWPath("PATCH", kldevents.StreamPathPrefix, nil, nil)
+	assert.Equal(405, res.Result().StatusCode)
+}
+
+func TestUpdateStreamOK(t *testing.T) {
+	assert := assert.New(t)
+	spec := &kldevents.StreamInfo{Type: "webhook", Name: "stream-new-name"}
+	b, _ := json.Marshal(spec)
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	res := httptest.NewRecorder()
+	s := &smartContractGW{}
+	s.sm = &mockSubMgr{}
+	r := &httprouter.Router{}
+	s.AddRoutes(r)
+	r.ServeHTTP(res, req)
+	var newSpec kldevents.StreamInfo
+	json.NewDecoder(res.Body).Decode(&newSpec)
+	assert.Equal(200, res.Result().StatusCode)
+	s.Shutdown()
+}
+
+func TestUpdateStreamBadData(t *testing.T) {
+	assert := assert.New(t)
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader([]byte(":bad json")))
+	res := httptest.NewRecorder()
+	s := &smartContractGW{}
+	s.sm = &mockSubMgr{}
+	r := &httprouter.Router{}
+	s.AddRoutes(r)
+	r.ServeHTTP(res, req)
+	var resError restErrMsg
+	json.NewDecoder(res.Body).Decode(&resError)
+	assert.Equal(400, res.Result().StatusCode)
+	assert.Regexp("Invalid event stream specification", resError.Message)
+}
+
+func TestUpdateStreamNotFoundError(t *testing.T) {
+	assert := assert.New(t)
+	spec := &kldevents.StreamInfo{Type: "webhook"}
+	b, _ := json.Marshal(spec)
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	res := httptest.NewRecorder()
+	s := &smartContractGW{}
+	s.sm = &mockSubMgr{err: fmt.Errorf("pop")}
+	r := &httprouter.Router{}
+	s.AddRoutes(r)
+	r.ServeHTTP(res, req)
+	var resError restErrMsg
+	json.NewDecoder(res.Body).Decode(&resError)
+	assert.Equal(404, res.Result().StatusCode)
+	assert.Regexp("pop", resError.Message)
+}
+
+func TestUpdateStreamSubMgrError(t *testing.T) {
+	assert := assert.New(t)
+	spec := &kldevents.StreamInfo{Type: "webhook"}
+	b, _ := json.Marshal(spec)
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	res := httptest.NewRecorder()
+	s := &smartContractGW{}
+	s.sm = &mockSubMgr{
+		updateStreamErr: fmt.Errorf("pop"),
+		err:             nil,
+	}
+	r := &httprouter.Router{}
+	s.AddRoutes(r)
+	r.ServeHTTP(res, req)
+	var resError restErrMsg
+	json.NewDecoder(res.Body).Decode(&resError)
+	assert.Equal(500, res.Result().StatusCode)
+	assert.Regexp("pop", resError.Message)
+}
+
 func TestListStreamsNoSubMgr(t *testing.T) {
 	assert := assert.New(t)
 	res := testGWPath("GET", kldevents.StreamPathPrefix, nil, nil)
@@ -1681,12 +1756,12 @@ func TestListSubs(t *testing.T) {
 
 	mockSubMgr := &mockSubMgr{
 		subs: []*kldevents.SubscriptionInfo{
-			&kldevents.SubscriptionInfo{
+			{
 				TimeSorted: kldmessages.TimeSorted{
 					CreatedISO8601: time.Now().UTC().Format(time.RFC3339),
 				}, ID: "earlier",
 			},
-			&kldevents.SubscriptionInfo{
+			{
 				TimeSorted: kldmessages.TimeSorted{
 					CreatedISO8601: time.Now().UTC().Add(1 * time.Hour).Format(time.RFC3339),
 				}, ID: "later",

Makefile
@@ -12,7 +12,10 @@ build:
 		$(VGO) build -ldflags "-X main.buildDate=`date -u +\"%Y-%m-%dT%H:%M:%SZ\"` -X main.buildVersion=$(BUILD_VERSION)" -tags=prod -o $(BINARY_NAME) -v
 coverage.txt: $(GOFILES)
 		$(VGO) test  ./... -cover -coverprofile=coverage.txt -covermode=atomic
+coverage.html:
+	    $(VGO) tool cover -html=coverage.txt
 test: coverage.txt
+coverage: coverage.txt coverage.html
 clean: 
 		$(VGO) clean
 		rm -f coverage.txt

internal/kldcontracts/rest2eth_test.go
@@ -127,19 +127,23 @@ func (m *mockRPC) CallContext(ctx context.Context, result interface{}, method st
 }
 
 type mockSubMgr struct {
-	err          error
-	sub          *kldevents.SubscriptionInfo
-	stream       *kldevents.StreamInfo
-	subs         []*kldevents.SubscriptionInfo
-	streams      []*kldevents.StreamInfo
-	suspended    bool
-	resumed      bool
-	capturedAddr *kldbind.Address
+	err             error
+	updateStreamErr error
+	sub             *kldevents.SubscriptionInfo
+	stream          *kldevents.StreamInfo
+	subs            []*kldevents.SubscriptionInfo
+	streams         []*kldevents.StreamInfo
+	suspended       bool
+	resumed         bool
+	capturedAddr    *kldbind.Address
 }
 
 func (m *mockSubMgr) Init() error { return m.err }
 func (m *mockSubMgr) AddStream(ctx context.Context, spec *kldevents.StreamInfo) (*kldevents.StreamInfo, error) {
 	return spec, m.err
+}
+func (m *mockSubMgr) UpdateStream(ctx context.Context, id string, spec *kldevents.StreamInfo) (*kldevents.StreamInfo, error) {
+	return m.stream, m.updateStreamErr
 }
 func (m *mockSubMgr) Streams(ctx context.Context) []*kldevents.StreamInfo { return m.streams }
 func (m *mockSubMgr) StreamByID(ctx context.Context, id string) (*kldevents.StreamInfo, error) {

internal/kldevents/eventstream.go
@@ -80,7 +80,6 @@ type eventStream struct {
 	spec              *StreamInfo
 	eventStream       chan *eventData
 	stopped           bool
-	dispatcherDone    bool
 	processorDone     bool
 	pollingInterval   time.Duration
 	pollerDone        bool
@@ -90,6 +89,9 @@ type eventStream struct {
 	batchCount        uint64
 	initialRetryDelay time.Duration
 	backoffFactor     float64
+	updateInProgress  bool
+	updateInterrupt   chan struct{}   // a zero-sized struct used only for signaling (hand rolled alternative to context)
+	updateWG          *sync.WaitGroup // Wait group for the go routines to reply back after they have stopped
 }
 
 // newEventStream constructor verfies the action is correct, kicks
@@ -150,15 +152,94 @@ func newEventStream(sm subscriptionManager, spec *StreamInfo) (a *eventStream, e
 		// Let's us do this from UTs, without exposing it
 		a.pollingInterval = 10 * time.Millisecond
 	}
-	go a.eventPoller()
-	go a.batchProcessor()
-	go a.batchDispatcher()
+	a.startEventHandlers(false)
 	return a, nil
+}
+
+// helper to kick off go routines and any tracking entities
+func (a *eventStream) startEventHandlers(resume bool) {
+	// create a context that can be used to indicate an update to the eventstream
+	a.updateInterrupt = make(chan struct{})
+	a.updateWG = &sync.WaitGroup{}
+	a.updateWG.Add(1) // add a channel for eventPoller to inform after it has stopped
+	go a.eventPoller()
+	a.updateWG.Add(1) // add a channel for batchProcessor to inform after it has stopped
+	go a.batchProcessor()
+	// For a pause/resume, the batch dispatcher goroutine is not terminated, hence no need to start it
+	if !resume {
+		a.updateWG.Add(1) // add a channel for batchDispatcher to inform after it has stopped
+		go a.batchDispatcher()
+	}
 }
 
 // GetID returns the ID (for sorting)
 func (spec *StreamInfo) GetID() string {
 	return spec.ID
+}
+
+// preUpdateStream sets a flag to indicate updateInProgress and wakes up goroutines waiting on condition variable
+func (a *eventStream) preUpdateStream() {
+	a.batchCond.L.Lock()
+	a.updateInProgress = true
+	a.batchCond.Broadcast()
+	a.batchCond.L.Unlock()
+}
+
+// postUpdateStream resets flags and kicks off a fresh round of handler go routines
+func (a *eventStream) postUpdateStream() {
+	a.batchCond.L.Lock()
+	a.pollerDone = false
+	a.processorDone = false
+	a.updateInProgress = false
+	a.batchCond.L.Unlock()
+	a.startEventHandlers(false)
+}
+
+// update modifies an existing eventStream
+func (a *eventStream) update(newSpec *StreamInfo) (spec *StreamInfo, err error) {
+	log.Infof("%s: Update event stream", a.spec.ID)
+	// set a flag to indicate updateInProgress
+	// For any go routines that are Wait() ing on the eventListener, wake them up
+	a.preUpdateStream()
+	// close the updateInterrupt channel so that the event handler go routines can be woken up
+	close(a.updateInterrupt)
+	// wait for the poked goroutines to finish up
+	a.updateWG.Wait()
+
+	if a.spec.BatchSize != newSpec.BatchSize && newSpec.BatchSize != 0 && newSpec.BatchSize < MaxBatchSize {
+		a.spec.BatchSize = newSpec.BatchSize
+	}
+	if a.spec.BatchTimeoutMS != newSpec.BatchTimeoutMS && newSpec.BatchTimeoutMS != 0 {
+		a.spec.BatchTimeoutMS = newSpec.BatchTimeoutMS
+	}
+	if a.spec.BlockedRetryDelaySec != newSpec.BlockedRetryDelaySec && newSpec.BlockedRetryDelaySec != 0 {
+		a.spec.BlockedRetryDelaySec = newSpec.BlockedRetryDelaySec
+	}
+	if strings.ToLower(newSpec.ErrorHandling) == ErrorHandlingBlock {
+		a.spec.ErrorHandling = ErrorHandlingBlock
+	} else {
+		a.spec.ErrorHandling = ErrorHandlingSkip
+	}
+	if a.spec.Name != newSpec.Name {
+		a.spec.Name = newSpec.Name
+	}
+	if newSpec.Webhook != nil {
+		if newSpec.Webhook.URL == "" {
+			return nil, klderrors.Errorf(klderrors.EventStreamsWebhookNoURL)
+		}
+		if _, err = url.Parse(newSpec.Webhook.URL); err != nil {
+			return nil, klderrors.Errorf(klderrors.EventStreamsWebhookInvalidURL)
+		}
+		if newSpec.Webhook.RequestTimeoutSec == 0 {
+			newSpec.Webhook.RequestTimeoutSec = 30000
+		}
+		a.spec.Webhook.URL = newSpec.Webhook.URL
+		a.spec.Webhook.RequestTimeoutSec = newSpec.Webhook.RequestTimeoutSec
+		a.spec.Webhook.TLSkipHostVerify = newSpec.Webhook.TLSkipHostVerify
+		a.spec.Webhook.Headers = newSpec.Webhook.Headers
+	}
+	a.postUpdateStream()
+	return a.spec, nil
 }
 
 // HandleEvent is the entry point for the stream from the event detection logic
@@ -172,12 +253,12 @@ func (a *eventStream) handleEvent(event *eventData) {
 func (a *eventStream) stop() {
 	a.batchCond.L.Lock()
 	a.stopped = true
-	a.eventStream <- nil
+	close(a.eventStream)
 	a.batchCond.Broadcast()
 	a.batchCond.L.Unlock()
 }
 
-// suspend only stops the dispatcher, pushing back as if we're in blockding mode
+// suspend only stops the dispatcher, pushing back as if we're in blocking mode
 func (a *eventStream) suspend() {
 	a.batchCond.L.Lock()
 	a.spec.Suspended = true
@@ -195,13 +276,13 @@ func (a *eventStream) resume() error {
 	a.spec.Suspended = false
 	a.processorDone = false
 	a.pollerDone = false
-	go a.eventPoller()
-	go a.batchProcessor()
+
+	a.startEventHandlers(true)
 	a.batchCond.Broadcast()
 	return nil
 }
 
-// isBlocked protect us from poling for more events when the stream is blocked.
+// isBlocked protect us from polling for more events when the stream is blocked.
 // Can happen regardless of whether the error handling is
 // block or skip. It's just with skip we eventually move onto new messages
 // after the retries etc. are complete
@@ -219,6 +300,7 @@ func (a *eventStream) isBlocked() bool {
 // eventPoller checks every few seconds against the ethereum node for any
 // new events on the subscriptions that are registered for this stream
 func (a *eventStream) eventPoller() {
+	defer a.updateWG.Done()
 
 	ctx := kldauth.NewSystemAuthContext()
 
@@ -272,11 +354,19 @@ func (a *eventStream) eventPoller() {
 				}
 			}
 		}
-		time.Sleep(a.pollingInterval)
+		// the event poller reacts to notification about a stream update, else it starts
+		// another round of polling after completion of the pollingInterval
+		select {
+		case <-a.updateInterrupt:
+			// we were notified by the caller about an ongoing update, no need to continue
+			log.Infof("%s: Notified of an ongoing stream update, exiting event poller", a.spec.ID)
+			return
+		case <-time.After(a.pollingInterval): //fall through and continue to the next iteration
+		}
 	}
 }
 
-// batchDispatcher is the goroutine that is alsway available to read new
+// batchDispatcher is the goroutine that is always available to read new
 // events and form them into batches. Because we can't be sure how many
 // events we'll be dispatched from blocks before the IsBlocked() feedback
 // loop protects us, this logic has to build a list of batches
@@ -284,10 +374,11 @@ func (a *eventStream) batchDispatcher() {
 	var currentBatch []*eventData
 	var batchStart time.Time
 	batchTimeout := time.Duration(a.spec.BatchTimeoutMS) * time.Millisecond
-	defer func() { a.dispatcherDone = true }()
+	defer a.updateWG.Done()
 	for {
 		// Wait for the next event - if we're in the middle of a batch, we
 		// need to cope with a timeout
+		log.Debugf("%s: Begin batch dispatcher loop, current batch length: %d", a.spec.ID, len(currentBatch))
 		timeout := false
 		if len(currentBatch) > 0 {
 			// Existing batch
@@ -304,16 +395,28 @@ func (a *eventStream) batchDispatcher() {
 					return
 				}
 				currentBatch = append(currentBatch, event)
-			}
-		} else {
-			// New batch
-			event := <-a.eventStream
-			if event == nil {
-				log.Infof("%s: Event stream stopped", a.spec.ID)
+			case <-a.updateInterrupt:
+				// we were notified by the caller about an ongoing update, cancel the timeout ctx and return
+				log.Infof("%s: Notified of an ongoing stream update, will not dispatch batch", a.spec.ID)
+				cancel() // cancel the ctx which was started to track timeout
 				return
 			}
-			currentBatch = []*eventData{event}
-			batchStart = time.Now()
+		} else {
+			// New batch - react to an update notification or process the next set of events from the stream
+			select {
+			case <-a.updateInterrupt:
+				// we were notified by the caller about an ongoing update, return
+				log.Infof("%s: Notified of an ongoing stream update, not waiting for new events", a.spec.ID)
+				return
+			case event := <-a.eventStream:
+				if event == nil {
+					log.Infof("%s: Event stream stopped", a.spec.ID)
+					return
+				}
+				currentBatch = []*eventData{event}
+				log.Infof("%s: New batch length %d", a.spec.ID, len(currentBatch))
+				batchStart = time.Now()
+			}
 		}
 		if timeout || uint64(len(currentBatch)) == a.spec.BatchSize {
 			// We are ready to dispatch the batch
@@ -348,9 +451,21 @@ func (a *eventStream) batchProcessor() {
 		// Wait for the next batch, or to be stopped
 		a.batchCond.L.Lock()
 		for !a.suspendOrStop() && a.batchQueue.Len() == 0 {
-			a.batchCond.Wait()
+			if a.updateInProgress {
+				select {
+				case <-a.updateInterrupt:
+					// we were notified by the caller about an ongoing update, return
+					log.Infof("%s: Notified of an ongoing stream update, exiting batch processor", a.spec.ID)
+					a.updateWG.Done() //Not moving this to a 'defer' since we need to unlock after calling Done()
+					a.batchCond.L.Unlock()
+					return
+				}
+			} else {
+				a.batchCond.Wait()
+			}
 		}
 		if a.suspendOrStop() {
+			log.Infof("%s: Suspended, returning exiting batch processor", a.spec.ID)
 			a.batchCond.L.Unlock()
 			return
 		}
@@ -361,6 +476,8 @@ func (a *eventStream) batchProcessor() {
 		a.batchCond.L.Unlock()
 		// Process the batch - could block for a very long time, particularly if
 		// ErrorHandlingBlock is configured.
+		// Track this as an item in the update wait group
+		a.updateWG.Add(1)
 		a.processBatch(batchNumber, batchElem.Value.([]*eventData))
 	}
 }
@@ -369,6 +486,7 @@ func (a *eventStream) batchProcessor() {
 // It never returns an error, and uses the chosen block/skip ErrorHandling
 // behaviour combined with the parameters on the event itself
 func (a *eventStream) processBatch(batchNumber uint64, events []*eventData) {
+	defer a.updateWG.Done()
 	if len(events) == 0 {
 		return
 	}
@@ -376,10 +494,17 @@ func (a *eventStream) processBatch(batchNumber uint64, events []*eventData) {
 	attempt := 0
 	for !a.suspendOrStop() && !processed {
 		if attempt > 0 {
-			time.Sleep(time.Duration(a.spec.BlockedRetryDelaySec) * time.Second)
+			select {
+			case <-a.updateInterrupt:
+				// we were notified by the caller about an ongoing update, no need to continue
+				log.Infof("%s: Notified of an ongoing stream update, terminating process batch", a.spec.ID)
+				return
+			case <-time.After(time.Duration(a.spec.BlockedRetryDelaySec) * time.Second): //fall through and continue
+			}
 		}
 		attempt++
 		log.Infof("%s: Batch %d initiated with %d events. FirstBlock=%s LastBlock=%s", a.spec.ID, batchNumber, len(events), events[0].BlockNumber, events[len(events)-1].BlockNumber)
+		a.updateWG.Add(1)
 		err := a.performActionWithRetry(batchNumber, events)
 		// If we got an error after all of the internal retries within the event
 		// handler failed, then the ErrorHandling strategy kicks in
@@ -391,9 +516,11 @@ func (a *eventStream) processBatch(batchNumber uint64, events []*eventData) {
 		}
 	}
 
-	// Always decrement the in-flight count once we've processed
+	// decrement the in-flight count if we've processed (wouldn't have occurred if we were suspended or stopped)
 	a.batchCond.L.Lock()
-	a.inFlight -= uint64(len(events))
+	if processed {
+		a.inFlight -= uint64(len(events))
+	}
 	a.batchCond.L.Unlock()
 
 	// If we were suspended, do not ack the batch
@@ -421,10 +548,18 @@ func (a *eventStream) performActionWithRetry(batchNumber uint64, events []*event
 	delay := a.initialRetryDelay
 	var attempt uint64
 	complete := false
+	defer a.updateWG.Done()
+
 	for !a.suspendOrStop() && !complete {
 		if attempt > 0 {
 			log.Infof("%s: Waiting %.2fs before re-attempting batch %d", a.spec.ID, delay.Seconds(), batchNumber)
-			time.Sleep(delay)
+			select {
+			case <-a.updateInterrupt:
+				// we were notified by the caller about an ongoing update, no need to continue
+				log.Infof("%s: Notified of an ongoing stream update, terminating perform action for batch number: %d", a.spec.ID, batchNumber)
+				return
+			case <-time.After(delay): //fall through and continue
+			}
 			delay = time.Duration(float64(delay) * a.backoffFactor)
 		}
 		attempt++

commit 05bf397441c0d5856bb79554f294e420ea9be145

Fix URL to include ID as parameter in update stream
Bug introduced in: https://github.com/kaleido-io/ethconnect/pull/73

internal/kldcontracts/smartcontractgw.go
@@ -115,7 +115,7 @@ func (g *smartContractGW) AddRoutes(router *httprouter.Router) {
 	router.GET("/gateways/:gateway_lookup", g.getRemoteRegistrySwaggerOrABI)
 	router.GET("/g/:gateway_lookup", g.getRemoteRegistrySwaggerOrABI)
 	router.POST(kldevents.StreamPathPrefix, g.withEventsAuth(g.createStream))
-	router.PATCH(kldevents.StreamPathPrefix, g.withEventsAuth(g.updateStream))
+	router.PATCH(kldevents.StreamPathPrefix+"/:id", g.withEventsAuth(g.updateStream))
 	router.GET(kldevents.StreamPathPrefix, g.withEventsAuth(g.listStreamsOrSubs))
 	router.GET(kldevents.SubPathPrefix, g.withEventsAuth(g.listStreamsOrSubs))
 	router.GET(kldevents.StreamPathPrefix+"/:id", g.withEventsAuth(g.getStreamOrSub))

internal/kldcontracts/smartcontractgw_test.go
@@ -1666,15 +1666,15 @@ func TestAddStreamSubMgrError(t *testing.T) {
 
 func TestUpdateStreamNoSubMgr(t *testing.T) {
 	assert := assert.New(t)
-	res := testGWPath("PATCH", kldevents.StreamPathPrefix, nil, nil)
+	res := testGWPath("PATCH", kldevents.StreamPathPrefix+"/123", nil, nil)
 	assert.Equal(405, res.Result().StatusCode)
 }
 
 func TestUpdateStreamOK(t *testing.T) {
 	assert := assert.New(t)
-	spec := &kldevents.StreamInfo{Type: "webhook", Name: "stream-new-name"}
+	spec := &kldevents.StreamInfo{Type: "webhook", Name: "stream-new-name", ID: "123"}
 	b, _ := json.Marshal(spec)
-	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix+"/123", bytes.NewReader(b))
 	res := httptest.NewRecorder()
 	s := &smartContractGW{}
 	s.sm = &mockSubMgr{}
@@ -1689,7 +1689,7 @@ func TestUpdateStreamOK(t *testing.T) {
 
 func TestUpdateStreamBadData(t *testing.T) {
 	assert := assert.New(t)
-	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader([]byte(":bad json")))
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix+"/123", bytes.NewReader([]byte(":bad json")))
 	res := httptest.NewRecorder()
 	s := &smartContractGW{}
 	s.sm = &mockSubMgr{}
@@ -1704,9 +1704,9 @@ func TestUpdateStreamBadData(t *testing.T) {
 
 func TestUpdateStreamNotFoundError(t *testing.T) {
 	assert := assert.New(t)
-	spec := &kldevents.StreamInfo{Type: "webhook"}
+	spec := &kldevents.StreamInfo{Type: "webhook", ID: "123"}
 	b, _ := json.Marshal(spec)
-	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix+"/123", bytes.NewReader(b))
 	res := httptest.NewRecorder()
 	s := &smartContractGW{}
 	s.sm = &mockSubMgr{err: fmt.Errorf("pop")}
@@ -1721,9 +1721,9 @@ func TestUpdateStreamNotFoundError(t *testing.T) {
 
 func TestUpdateStreamSubMgrError(t *testing.T) {
 	assert := assert.New(t)
-	spec := &kldevents.StreamInfo{Type: "webhook"}
+	spec := &kldevents.StreamInfo{Type: "webhook", ID: "123"}
 	b, _ := json.Marshal(spec)
-	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix, bytes.NewReader(b))
+	req := httptest.NewRequest("PATCH", kldevents.StreamPathPrefix+"/123", bytes.NewReader(b))
 	res := httptest.NewRecorder()
 	s := &smartContractGW{}
 	s.sm = &mockSubMgr{
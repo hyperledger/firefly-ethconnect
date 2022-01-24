a432244394290ff0f08ea322fbacbcd87c78d4b9
Hayden Fuss <haydenfuss@gmail.com>
2022-01-24 13:47:13 -0500

    Fixing Graceful Shutdown for Container Image (#195)
    Signed-off-by: hfuss <haydenfuss@gmail.com>

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+args=""
 if [ "$#" -eq 0 ]; then
     if [ -n "${ETHCONNECT_CONFIGFILE}" ]; then
-        ethconnect server
+        args="server"
     else
         echo "ETHCONNECT_CONFIGFILE unset, and no commandline parameters. Running with default standalone configuration"
-        ethconnect rest -U http://127.0.0.1:8080 -I ./abis -r http://127.0.0.1:8545 -E ./events
+        args="rest -U http://127.0.0.1:8080 -I ./abis -r http://127.0.0.1:8545 -E ./events"
     fi
 else
-    ethconnect $@
+    args="$@"
 fi
+
+exec ethconnect $args

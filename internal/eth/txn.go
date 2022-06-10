a8eeea0f1edf7ba1aeacca0e89c51339a64a16fd
Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>
2022-06-10 22:01:41 +0530

    signed off
    Signed-off-by: ShubhamSolanki24 shubham24071999@gmail.com
    
    Signed-off-by: Shubham Solanki <49410636+ShubhamSolanki24@users.noreply.github.com>

--- internal/eth/txn.go
+++ internal/eth/txn.go
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the license for the specific language governing permissions and
 // limitations under the License.
 
 package eth
